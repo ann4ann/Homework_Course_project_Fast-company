584fcdda206f8aedd1dce91b4bc4926385a0660c
Anna Noskova <anni4anny@gmail.com>
2021-10-06 11:59:35 +0300

    Creating form - login

---
+++ src/app/components/textField.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const TextField = ({ label, type, name, value, onChange }) => {
+    return (
+        <div>
+            <label htmlFor={name}>{label}</label>
+            <input
+                type={type}
+                id={name}
+                name={name}
+                value={value}
+                onChange={onChange}
+            />
+        </div>
+    );
+};
+
+TextField.defaultProps = {
+    type: "text"
+};
+TextField.propTypes = {
+    label: PropTypes.string,
+    type: PropTypes.string,
+    name: PropTypes.string,
+    value: PropTypes.string,
+    onChange: PropTypes.func
+};
+
+export default TextField;
--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,7 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import TextField from "../components/textField";
 
 const Login = () => {
-    return <h1>Login</h1>;
+    const [data, setData] = useState({ email: "", password: "" });
+    // чтобы контролировать несколько полей, можно создать для каждого useState, но это не оптимально
+    // const [password, setPassword] = useState("");
+    // const handleChange = (e) => {
+    //     setEmail(e.target.value);
+    //     // console.log(e.target.value, "changed");
+    // };
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }));
+        console.log(target.name, target.value);
+    };
+    const handleSubmit = (e) => {
+        // отменяем обновление страницы (действие по умолчанию) при нажатии кнопки
+        e.preventDefault();
+        // console.log(e); // -> target -> action: "http://localhost:3000/login" - то, что отменили с пом. preventDefault()
+        console.log(data); // вернет объект со значениями из формы
+        // !!!!! есть нюанс - может сработать submit до того, как мы получим все значения из полей
+    };
+
+    return (
+        // доступ к значениям полей получаем через атрибут value или onChange
+        // value - то значение, которое передаем в поле
+        // onChange - даные об изменении содержания поля
+        // контролируемое поле в Реакт - когда в нем есть value и onChange
+        // Неконтролируемое - без них
+        <form onSubmit={handleSubmit}>
+            <TextField
+                label="Электронная почта"
+                name="email"
+                value={data.email}
+                onChange={handleChange}
+            />
+            <TextField
+                label="Пароль"
+                type="password"
+                name="password"
+                value={data.password}
+                onChange={handleChange}
+            />
+            {/* кнопке необязательно добавлять type:"submit", т.к. он у всех кнопок в форме по дефолту
+            если кнапка не submit, ей необходимо добавить type:"button" */}
+            <button type="submit">Submit</button>
+            {/* <div>
+                <label htmlFor="email">Email</label>
+                <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    value={data.email}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={data.password}
+                    onChange={handleChange}
+                />
+            </div> */}
+            {/* <div>
+                <div>
+                    <label htmlFor="radio1">Radio 1</label>
+                    <input type="radio" id="radio1" name="radio" />
+                </div>
+                <div>
+                    <label htmlFor="radio2">Radio 2</label>
+                    <input type="radio" id="radio2" name="radio" />
+                </div>
+            </div> */}
+        </form>
+    );
 };
 
 export default Login;
